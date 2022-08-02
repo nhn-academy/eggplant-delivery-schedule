cff7b4e0f68c645d1bde3999dcc906c26edb2299
조재철 <rhsnqk@gmail.com>
2022-08-02 23:28:07 +0900

    feat: Spring Batch 설정 (#3)
    Co-authored-by: @bunsung92 <bunsung92@naver.com>"

---
+++ src/main/java/com/nhnacademy/eggplantdeliveryschedule/config/BatchConfig.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.eggplantdeliveryschedule.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Batch 설정 클래스.
+ *
+ * @author : 조재철
+ * @version 1.0
+ */
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final JobRegistry jobRegistry;
+
+    /**
+     * Job 마다 등록할 필요 없이 Job Type 이 빈으로 등록될 때 후 처리로 자동으로 JobRegistry 에 등록
+     * JobRegistryBeanPostProcessor 에 JobRegistry 를 등록하게 하는 클래스.
+     *
+     * @return JobRegistry 를 등록한 JobPostBeanProcessor 반환.
+     */
+    @Bean
+    public BeanPostProcessor beanPostProcessor() {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+
+        return jobRegistryBeanPostProcessor;
+    }
+}

54847f35aa8ff38d0aa3a368f20d1bfeedb93f3f
조재철 <rhsnqk@gmail.com>
2022-08-05 21:40:05 +0900

    entity 작성 (#9)
    Co-authored-by: @bunsung92 <bunsung92@naver.com>

---
+++ src/main/java/com/nhnacademy/eggplantdeliveryschedule/entity/Location.java
@@ -0,0 +1,35 @@
+package com.nhnacademy.eggplantdeliveryschedule.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.NoArgsConstructor;
+
+/**
+ * 배송위치 엔티티 입니다.
+ *
+ * @author 김훈민, 조재철
+ * @version 1.0.0
+ */
+@Entity
+@Table(name = "location")
+@NoArgsConstructor
+public class Location {
+
+    @Id
+    @Column(name = "location_no")
+    private Long locationNo;
+
+    @ManyToOne
+    @JoinColumn(name = "tracking_no")
+    private DeliveryInfo deliveryInfo;
+
+    @Column(name = "arrival_time")
+    private LocalDateTime arrivalTime;
+
+}
+

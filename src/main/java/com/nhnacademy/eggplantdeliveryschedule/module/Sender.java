30023bd86821745af15c7face4e6b3eb3dd4a57a
조재철 <rhsnqk@gmail.com>
2022-08-05 13:38:18 +0900

    feat : 배송 완료시 해당 서버에 전송할 데이터를 message queue에 넣기 위한 sender 구현 (#3)
    Co-authored-by: @bunsung92 <bunsung92@naver.com>

---
+++ src/main/java/com/nhnacademy/eggplantdeliveryschedule/module/Sender.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.eggplantdeliveryschedule.module;
+
+import com.nhnacademy.eggplantdeliveryschedule.dto.ChangedDeliveryInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+@RequiredArgsConstructor
+@Component
+public class Sender {
+
+    private static final String DELIVERY_EXCHANGE = "exchange.direct";
+    public static final String ROUTING_DELIVERY_ARRIVAL = "routing.DeliveryArrival";
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendDeliveryArrival(final ChangedDeliveryInfoDto changedDeliveryInfoDto) {
+        rabbitTemplate.convertAndSend(DELIVERY_EXCHANGE, ROUTING_DELIVERY_ARRIVAL, changedDeliveryInfoDto);
+    }
+}

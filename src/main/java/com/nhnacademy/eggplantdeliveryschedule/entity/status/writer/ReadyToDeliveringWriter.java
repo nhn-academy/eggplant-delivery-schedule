15b212a5a2aad0df2a24805bbb3f6cd5b52a1bbc
bunsung92 <bunsung92@naver.com>
2022-08-07 19:09:10 +0900

    chore: sonar critical 이슈 해결(#17)
    Co-authored-by: @JoisFe <rhsnqk@gmail.com>

--- src/main/java/com/nhnacademy/eggplantdeliveryschedule/entity/status/writer/ReadyToDeliveringWriter.java
+++ src/main/java/com/nhnacademy/eggplantdeliveryschedule/entity/status/writer/ReadyToDeliveringWriter.java
@@ -1,7 +1,8 @@
-package com.nhnacademy.eggplantdeliveryschedule.writer;
+package com.nhnacademy.eggplantdeliveryschedule.entity.status.writer;
 
 import com.nhnacademy.eggplantdeliveryschedule.entity.DeliveryInfo;
 import com.nhnacademy.eggplantdeliveryschedule.entity.Location;
+import com.nhnacademy.eggplantdeliveryschedule.entity.status.Status;
 import com.nhnacademy.eggplantdeliveryschedule.exception.NotFoundDeliveryInfoException;
 import com.nhnacademy.eggplantdeliveryschedule.repository.DeliveryInfoRepository;
 import com.nhnacademy.eggplantdeliveryschedule.repository.LocationRepository;
@@ -9,20 +10,31 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
 @Component
 @StepScope
 @RequiredArgsConstructor
-public class DeliveryLocationChangeWriter implements ItemWriter<List<String>> {
+public class ReadyToDeliveringWriter implements ItemWriter<List<String>> {
 
+    private StepExecution stepExecution;
     private final DeliveryInfoRepository deliveryInfoRepository;
     private final LocationRepository locationRepository;
 
     @Override
     public void write(List<? extends List<String>> trackingNoChunkList) {
+        ExecutionContext executionContext = this.stepExecution.getExecutionContext();
+        executionContext.put("trackingNoChunkList", trackingNoChunkList);
+
         List<String> trackingNoList = trackingNoChunkList.stream()
                                                          .flatMap(List::stream)
                                                          .collect(Collectors.toList());
@@ -30,14 +42,21 @@ public class DeliveryLocationChangeWriter implements ItemWriter<List<String>> {
         for (String trackingNo : trackingNoList) {
             DeliveryInfo deliveryInfo = deliveryInfoRepository.findById(trackingNo)
                                                               .orElseThrow(NotFoundDeliveryInfoException::new);
+            deliveryInfo.updateStatus(Status.DELIVERING);
+
             Location location = Location.builder()
-                                        .pk(new Location.Pk(2L, deliveryInfo.getTrackingNo()))
+                                        .pk(new Location.Pk(1L, deliveryInfo.getTrackingNo()))
                                         .deliveryInfo(deliveryInfo)
                                         .arrivalTime(LocalDateTime.now())
                                         .build();
 
             locationRepository.save(location);
         }
+    }
+
+    @BeforeStep
+    public void saveStepExecution(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
     }
 
 }
